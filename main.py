5e4ba34791f0aa7fdead4c3ec8c0dfa428eecf63
shehio <shehabyasser@gmail.com>
2023-01-29 11:49:26 -0800

    Added some deps

---
+++ main.py
@@ -0,0 +1,22 @@
+from __future__ import print_function, division
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import sys
+import os
+import os.path as path
+from os import listdir
+from os.path import isfile, join
+import copy
+import pickle
+from tqdm import tqdm
+import argparse
+from collections import namedtuple
+
+
+def main():
+    print('Hello World!')
+
+
+if __name__ == '__main__':
+    main()
